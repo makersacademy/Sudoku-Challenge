a9f919c88fc88b75c3dae5652dd8013954f9e138
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-02-02 11:29:09 +0000

    region can contain a square

--- lib/region.rb
+++ lib/region.rb
@@ -3,4 +3,12 @@ class Region
   def type
     :box
   end
+
+  def add(square)
+    @square = square
+  end
+
+  def squares
+    [@square]
+  end
 end
--- spec/region_spec.rb
+++ spec/region_spec.rb
@@ -2,9 +2,17 @@ require 'region'
 
 describe Region do
 
+  let(:region) { Region.new }
+  let(:square) { double :square }
+
   it 'is a row, column or box' do
      region = Region.new
      expect(region.type).to eq(:row).or eq(:column).or eq(:box) 
+  end
+
+  it 'can contain a square' do
+    region.add square
+    expect(region.squares).to include(square)
   end
 
 end
