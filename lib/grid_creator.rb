f4cfcc9a610f9257ab143e93182c7ac8ab8c300e
jamesascarter <jamesascarter@gmail.com>
2015-02-02 18:53:12 +0000

    final hound changes

--- lib/grid_creator.rb
+++ lib/grid_creator.rb
@@ -1,10 +1,9 @@
 module GridCreator
-
   BOX_OF_INDEX = [
-      0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,
-      3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,
-      6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8
-    ]
+    0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 0, 0, 1, 1, 1, 2, 2, 2, 0, 0, 0, 1, 1, 1, 2, 2, 2,
+    3, 3, 3, 4, 4, 4, 5, 5, 5, 3, 3, 3, 4, 4, 4, 5, 5, 5, 3, 3, 3, 4, 4, 4, 5, 5, 5,
+    6, 6, 6, 7, 7, 7, 8, 8, 8, 6, 6, 6, 7, 7, 7, 8, 8, 8, 6, 6, 6, 7, 7, 7, 8, 8, 8
+                 ]
 
   def create_rows
     @rows = cells.each_slice(9).to_a
@@ -17,7 +16,7 @@ module GridCreator
   def create_boxes
     @boxes = []
     iterator = BOX_OF_INDEX.each
-    9.times { @boxes << Array.new}
-    @cells.each{|cell| @boxes[iterator.next].concat([cell])}
+    9.times { @boxes << Array.new }
+    @cells.each { |cell| @boxes[iterator.next].concat([cell]) }
   end
 end
