131c4586b54094dae9a7e01171170eeabf454121
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-02-02 12:12:51 +0000

    game has board viewer

--- lib/game.rb
+++ lib/game.rb
@@ -4,4 +4,9 @@ class Game
     Board.new
   end
 
+  def show_board
+    viewer = BoardViewer.new
+    viewer.show(Board.new)
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,4 +8,8 @@ describe Game do
     expect(game.board).not_to be(nil)
   end
 
+  it 'can view the board' do
+    expect(game.show_board).to eq([[0]])
+  end
+
 end
