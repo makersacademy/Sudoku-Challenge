9d70d5bb68de5ea6c1ade10c54d1e0a2734435f1
Nick Dyer <nbdyer@gmail.com>
2015-02-04 14:33:10 +0000

    Adds sudoku grid to the view

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
