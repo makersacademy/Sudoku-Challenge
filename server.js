1c93875ebfcf6831ced87cbeca403771b1da1ff0
Nick Dyer <nbdyer@gmail.com>
2015-02-04 13:27:28 +0000

    Passing first feature step

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+"use strict";
 
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+server.listen(3000, function(){
+    console.log('Server listening on port 3000');
+});
+
+module.exports = server;
