01a918deb12e5d713a4b6d11dca4ace3372d8de1
Nick Dyer <nbdyer@gmail.com>
2015-02-05 15:17:38 +0000

    Refactor and adds test passing test against 0 values

--- public/js/cell.js
+++ public/js/cell.js
@@ -9,11 +9,26 @@
   };
 
   Cell.prototype.assignValue = function(number) {
-    if (number >= 10 ) { throw new TypeError ('Cell value must be 1-9.'); }
-    if (number != Math.abs(number)) { throw new TypeError ('Cell value must be positive.'); }
-    if (number != Math.floor(number)) { throw new TypeError ('Cell value must be an integer.'); }
+    this.CheckForInvalidValue(number);
     this.value = number;
   };
 
+  Cell.prototype.CheckForInvalidValue = function(number) {
+    if (this._isOutsideValidRange(number)) { throw new TypeError ('Cell value must be 1-9.'); }
+    if (this._isNegative(number)) { throw new TypeError ('Cell value must be positive.'); }
+    if (this._isFloat(number)) { throw new TypeError ('Cell value must be an integer.'); }
+  };
+
+  Cell.prototype._isOutsideValidRange = function(number) {
+    return number >= 10 || number === 0;
+  };
+
+  Cell.prototype._isNegative = function(number) {
+    return number != Math.abs(number);
+  };
+
+  Cell.prototype._isFloat = function(number) {
+    return number != Math.floor(number);
+  };
 
 module.exports = Cell;
--- specs/cellspec.js
+++ specs/cellspec.js
@@ -6,32 +6,37 @@ describe('Cell', function() {
 
   var cell;
 
+  beforeEach(function(done) {
+    cell = new Cell(); 
+    done();
+  });
+
   it ('is empty when created', function() {
-    cell = new Cell();
     expect(cell.isEmpty()).to.be.true;
   });
 
   it ('can be assigned a value', function() {
-    cell = new Cell();
     cell.assignValue(8);
     expect(cell.value).to.equal(8);
   });
 
   it ('can only be assigned a single digit value', function() {
-    cell = new Cell();
-    expect( function(){ cell.assignValue(10) } ).to.throw('Cell value must be 1-9.');
+    expect( function() { cell.assignValue(10) } ).to.throw('Cell value must be 1-9.');
     expect(cell.isEmpty()).to.be.true;
   });
 
   it ('can only be assigned a positive value', function() {
-    cell = new Cell();
-    expect( function(){ cell.assignValue(-9) } ).to.throw('Cell value must be positive.');
+    expect( function() { cell.assignValue(-9) } ).to.throw('Cell value must be positive.');
     expect(cell.isEmpty()).to.be.true;
   });
 
   it ('can only be assigned an integer value', function() {
-    cell = new Cell();
-    expect( function(){ cell.assignValue(4.2352) } ).to.throw('Cell value must be an integer.');
+    expect( function() { cell.assignValue(4.2352) } ).to.throw('Cell value must be an integer.');
+    expect(cell.isEmpty()).to.be.true;
+  });
+
+  it ('can not be assigned the value zero', function() {
+    expect( function() { cell.assignValue(0) } ).to.throw('Cell value must be 1-9.');
     expect(cell.isEmpty()).to.be.true;
   });
 
