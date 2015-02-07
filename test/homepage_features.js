afb2eb6043fad7534bd03267ebeaa4f27f7f0bd9
Edward Byne <ejbyne@gmail.com>
2015-02-07 23:13:16 +0000

    final feature test still not passing

--- test/homepage_features.js
+++ test/homepage_features.js
@@ -18,29 +18,33 @@ describe('homepage', function() {
 
 	it('has a form for entering the numbers', function() {
 		casper.then(function() {
-			expect('#number-form').to.be.inDOM;
+			expect('#number-form').to.be.inDOM.and.to.be.visible;
 		})
 	});
 
 	it('then provides a solution once the numbers have been entered', function() {
 		casper.then(function() {
-      this.fill('form[action="/solution"]', {
-      	A1: '5', A2: '3', A5: '7',
-      	B1: '6', B4: '1', B5: '9', B6: '5',
-      	C2: '9', C3: '8', C8: '6',
-      	D1: '8', D5: '6', D9: '3',
-      	E1: '4', E4: '8', E6: '3', E9: '6',
-      	F1: '7', F5: '2', F9: '6',
-      	G2: '6', G7: '2', G8: '8',
-      	H4: '4', H5: '1', H6: '9', H9: '5',
-      	I5: '8', I8: '7', I9: '9'
+      this.fillSelectors('form#number-form', {
+      	'input[name="A1"]': '5', 'input[name="A2"]': '3', 'input[name="A3"]': '', 'input[name="A4"]': '', 'input[name="A5"]': '7', 'input[name="A6"]': '', 'input[name="A7"]': '', 'input[name="A8"]': '', 'input[name="A9"]': '',
+      	'input[name="B1"]': '6', 'input[name="B2"]': '', 'input[name="B3"]': '', 'input[name="B4"]': '1', 'input[name="B5"]': '9', 'input[name="B6"]': '5', 'input[name="B7"]': '', 'input[name="B8"]': '', 'input[name="B9"]': '',
+      	'input[name="C1"]': '', 'input[name="C2"]': '9', 'input[name="C3"]': '8', 'input[name="C4"]': '', 'input[name="C5"]': '', 'input[name="C6"]': '', 'input[name="C7"]': '', 'input[name="C8"]': '6', 'input[name="C9"]': '',
+      	'input[name="D1"]': '8', 'input[name="D2"]': '', 'input[name="D3"]': '', 'input[name="D4"]': '', 'input[name="D5"]': '6', 'input[name="D6"]': '', 'input[name="D7"]': '', 'input[name="D8"]': '', 'input[name="D9"]': '3',
+      	'input[name="E1"]': '4', 'input[name="E2"]': '', 'input[name="E3"]': '', 'input[name="E4"]': '8', 'input[name="E5"]': '', 'input[name="E6"]': '3', 'input[name="E7"]': '', 'input[name="E8"]': '', 'input[name="E9"]': '1',
+      	'input[name="F1"]': '7', 'input[name="F2"]': '', 'input[name="F3"]': '', 'input[name="F4"]': '', 'input[name="F5"]': '2', 'input[name="F6"]': '', 'input[name="F7"]': '', 'input[name="F8"]': '', 'input[name="F9"]': '6',
+      	'input[name="G1"]': '', 'input[name="G2"]': '6', 'input[name="G3"]': '', 'input[name="G4"]': '', 'input[name="G5"]': '', 'input[name="G6"]': '', 'input[name="G7"]': '2', 'input[name="G8"]': '8', 'input[name="G9"]': '',
+      	'input[name="H1"]': '', 'input[name="H2"]': '', 'input[name="H3"]': '', 'input[name="H4"]': '4', 'input[name="H5"]': '1', 'input[name="H6"]': '9', 'input[name="H7"]': '', 'input[name="H8"]': '', 'input[name="H9"]': '5',
+      	'input[name="I1"]': '', 'input[name="I2"]': '', 'input[name="I3"]': '', 'input[name="I4"]': '', 'input[name="I5"]': '8', 'input[name="I6"]': '', 'input[name="I7"]': '', 'input[name="I8"]': '7', 'input[name="I9"]': '9'
       }, true);
-    });
-		casper.wait(1000, function() {
-      this.click('input[value="Solve!"]', function() {
-        expect('p').to.have.text('Go away:');
-        expect('#solution-table').to.be.inDOM;
-      });
-  	});
+		});
+		// casper.then(function() {
+		// 	this.click('form#number-form input[type="submit"]');
+		// });
+		casper.wait(2000, function() {
+			expect('body').to.contain.text('Solution:');
+		});
+		// casper.waitForSelector('#solution-table', function() {
+	 //    expect('body').to.contain.text('Solution:');
+		// });
 	});
+
 });
