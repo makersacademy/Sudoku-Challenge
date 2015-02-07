8d59d0a312bbf9870294ce13ef7db2c50eb01bc3
Edward Byne <ejbyne@gmail.com>
2015-02-07 14:17:13 +0000

    refactors controllers into separate file

---
+++ app/controllers/controller.js
@@ -0,0 +1,29 @@
+var controller = function(app) {
+
+	var Board = require('../models/board.js');
+	var Solver = require('../models/solver.js');
+	var board;
+	var solver;
+	var rows;
+
+	app.get('/', function(request, response) {
+		board = new Board();
+		solver = new Solver();
+		board.createGrid();
+		rows = board.splitGridCoordsIntoRows();
+		response.render('index', {rows: rows});
+	});
+
+	app.post('/solution', function(request, response) {
+		var initialNumbers = request.body;
+		for (var coord in initialNumbers) {
+			if (board.grid[coord] && initialNumbers[coord] !== '')
+				board.insertInitialNumber(coord, parseInt(initialNumbers[coord]));
+		}
+		solver.solveBoard(board);
+		response.render('solution', {rows: rows, board: board});
+	});
+
+};
+
+module.exports = controller;
--- server.js
+++ server.js
@@ -3,35 +3,13 @@ var app = express();
 var server = require('http').createServer(app);
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var path = require('path');
-var Board = require('./src/board.js');
-var Solver = require('./src/solver.js');
-var board;
-var solver;
-var rows;
+var controller = require('./app/controllers/controller.js')(app);
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/app/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', function(request, response) {
-	board = new Board();
-	solver = new Solver();
-	board.createGrid();
-	rows = board.splitGridCoordsIntoRows();
-	response.render('index', {rows: rows});
-});
-
-app.post('/solution', function(request, response) {
-	var initialNumbers = request.body;
-	for (var coord in initialNumbers) {
-		if (board.grid[coord] && initialNumbers[coord] !== '')
-			board.insertInitialNumber(coord, parseInt(initialNumbers[coord]));
-	}
-	solver.solveBoard(board);
-	response.render('solution', {rows: rows, board: board});
-});
 
 var port = process.env.PORT || 3000;
 
