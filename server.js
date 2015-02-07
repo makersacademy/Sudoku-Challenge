13989eff6461879a5f832b83d762a83010e80887
Edward Byne <ejbyne@gmail.com>
2015-02-07 23:32:13 +0000

    corrects controller file

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var app = express();
 var server = require('http').createServer(app);
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var controller = require('./app/controllers/controller.js')(app);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+var controller = require('./app/controllers/controller.js')(app);
 
 var port = process.env.PORT || 3000;
 
