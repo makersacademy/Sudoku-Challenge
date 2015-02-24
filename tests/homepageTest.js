44f36e7bf39a07256765f7bc3bf61598ca48b2e3
Nick Dyer <nbdyer@gmail.com>
2015-02-24 17:53:34 +0000

    Adds test for solving sudoku

--- tests/homepageTest.js
+++ tests/homepageTest.js
@@ -68,5 +68,16 @@ describe('Visiting the homepage', function() {
       .call(done);
   });
 
+  it('solves a puzzle in the table', function(done) {
+    client
+      .setValue('#sudokustring', '53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79')
+      .click('#solve')
+      .waitForValue('/html/body/center/table/tbody/tr[1]/td[3]/input', 1000)
+      .getValue('/html/body/center/table/tbody/tr[1]/td[3]/input', function(err, value) {
+        expect(value).to.eql("4");
+      })
+      .call(done);
+  });
+
 });
 
